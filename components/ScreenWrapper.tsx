4e7d94a37c84826ed220fdf795c7feff977a7c25
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-11-17 23:17:22 +0200

    Added ScreenWrapper Component

---
+++ components/ScreenWrapper.tsx
@@ -0,0 +1,19 @@
+import { View } from "react-native";
+import React, { ReactNode } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+type ScreenWrapperProps = {
+  children: ReactNode;
+  bg?: string;
+};
+
+const ScreenWrapper: React.FC<ScreenWrapperProps> = ({ children, bg }) => {
+  const { top } = useSafeAreaInsets();
+  const paddingTop = top > 0 ? top + 5 : 30;
+
+  return (
+    <View style={{ flex: 1, paddingTop, backgroundColor: bg }}>{children}</View>
+  );
+};
+
+export default ScreenWrapper;
